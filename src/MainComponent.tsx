64bced2aa25be18afc6bb9fd38d91fefd6e04537
barbashev <barbashev@krista.ru>
2024-07-10 08:52:39 +0300

    refactor: change imports

--- src/MainComponent.tsx
+++ src/MainComponent.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import Search from "./Search";
+import Search from "./Search.js";
 
 const MainComponent = () => {
     const [searchValue, setSearch] = useState<string>("")
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
 import { createRoot } from "react-dom/client";
-import MainComponent from "./MainComponent";
+import MainComponent from "./MainComponent.js";
 
 const root = createRoot(document.querySelector("#root")!).render(<MainComponent />)
